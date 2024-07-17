5fe7d36c50b216770593c09dba37092dc7442bd5
Starlex <alexsiviper@gmail.com>
2024-07-17 20:41:56 +0300

    added: models registered to admin panel

--- backend/client_panel/admin.py
+++ backend/client_panel/admin.py
@@ -1,3 +1,69 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name', 'parent', 'discount', 'is_show']
+    
+    search_fields = ['name', 'parent']
+    list_filter = ['name']
+    ordering = ['name']
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    def get_list_display(self, request):
+        return [field.name for field in Product._meta.get_fields()]
+    search_fields = ['name', 'code']
+    list_filter = ['name']
+    ordering = ['name']
+
+@admin.register(Characteristic)
+class CharacteristicAdmin(admin.ModelAdmin):
+    list_display = ['char_id', 'name', 'description']
+    search_fields = ['name']
+    list_filter = ['name']
+    ordering = ['name']
+
+@admin.register(Promotion)
+class PromotionAdmin(admin.ModelAdmin):
+    def get_list_display(self, request):
+        return [field.name for field in Promotion._meta.get_fields()]
+    search_fields = ['name']
+    list_filter = ['name']
+    ordering = ['name']
+
+@admin.register(PopularProduct)
+class PopularProductAdmin(admin.ModelAdmin):
+    def get_list_display(self, request):
+        return [field.name for field in PopularProduct._meta.get_fields()]
+    search_fields = ['category']
+    list_filter = ['category']
+    ordering = ['category']
+
+@admin.register(Gallery)
+class GalleryAdmin(admin.ModelAdmin):
+    def get_list_display(self, request):
+        return [field.name for field in Gallery._meta.get_fields()]
+    search_fields = ['name']
+    list_filter = ['name']
+    ordering = ['name']
+
+@admin.register(Contacts)
+class ContactsAdmin(admin.ModelAdmin):
+    list_display = ['opening_hours', 'closing_hours']
+    
+@admin.register(Phone)
+class PhoneAdmin(admin.ModelAdmin):
+    def get_list_display(self, request):
+        return [field.name for field in Phone._meta.get_fields()]
+    
+@admin.register(Email)
+class EmailAdmin(admin.ModelAdmin):
+    def get_list_display(self, request):
+        return [field.name for field in Email._meta.get_fields()]
+    
+@admin.register(Address)
+class AddressAdmin(admin.ModelAdmin):
+    def get_list_display(self, request):
+        return [field.name for field in Address._meta.get_fields()]
--- backend/client_panel/models.py
+++ backend/client_panel/models.py
@@ -6,7 +6,7 @@ from django.db.models import Index
 class Category(models.Model):
     name = models.CharField(max_length=255, blank=False, unique=True)
     parent = models.ForeignKey('self', null=True, blank=True, on_delete=models.CASCADE)
-    discount = models.IntegerField(null=True)
+    discount = models.IntegerField(null=True, blank=True)
     is_show = models.BooleanField(default=True, blank=False)
 
     def __str__(self):
@@ -16,6 +16,8 @@ class Category(models.Model):
             Index(fields=['name']),
             Index(fields=['is_show']),
         ]
+        verbose_name = "Category"
+        verbose_name_plural = "Categories"
 
 class Product(models.Model):
     product_id = models.IntegerField(primary_key=True)
@@ -26,7 +28,7 @@ class Product(models.Model):
     new_price = models.IntegerField(null=True, blank=True)
     code = models.CharField(max_length=255, null=True, blank=True)
     image = models.ImageField(upload_to=f'products/{category.name}/{name}/')
-    characteristic = models.ForeignKey('Characteristic')
+    characteristic = models.ForeignKey('Characteristic', on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name
@@ -37,25 +39,33 @@ class Product(models.Model):
             Index(fields=['category']),
             Index(fields=['characteristic']),
         ]
-
+        verbose_name = "Product"
+        verbose_name_plural = "Products"
 class Characteristic(models.Model):
     char_id = models.IntegerField(primary_key=True)
-    product = models.ForeignKey(Product, on_delete=models.CASCADE, null=True, blank=True)
     name = models.CharField(max_length=255, blank=False)
     description = models.CharField(max_length=255, blank=False)
 
     def __str__(self):
         return f'Название: {self.name}\n Описание: {self.description}'
+    
+    class Meta:
+        verbose_name = "Characteristic"
+        verbose_name_plural = "Characteristics"
 
 class Promotion(models.Model):
     name = models.CharField(max_length=255, blank=False)
     description = models.TextField(blank=False)
     is_show = models.BooleanField(default=False, blank=False)
-    main_image = models.ImageField(upload_to=f'promotions/{name[:20]}/main-image/')
-    second_image = models.ImageField(upload_to=f'promotions/{name[:20]}/second-image/')
+    main_image = models.ImageField(upload_to=f'promotions/{name}/main-image/')
+    second_image = models.ImageField(upload_to=f'promotions/{name}/second-image/')
 
     def __str__(self):
         return self.name
+    
+    class Meta:
+        verbose_name = "Promotion"
+        verbose_name_plural = "Promotions"
 
 class PopularProduct(models.Model):
     
@@ -66,28 +76,51 @@ class PopularProduct(models.Model):
     def __str__(self):
         return self.category.name
 
+    class Meta:
+        verbose_name = "PopularProduct"
+        verbose_name_plural = "PopularProducts"
 
 class Gallery(models.Model):
     gallery_id = models.IntegerField(primary_key=True)
     name = models.CharField(max_length=255, blank=False)
     description = models.TextField(blank=False)
-    image = models.ImageField(upload_to=f'gallery/{name[:20]}/')
+    image = models.ImageField(upload_to=f'gallery/{name}/')
 
     def __str__(self):
         return self.name
 
+    class Meta:
+        verbose_name = "Gallery"
+        verbose_name_plural = "Gallery"
 class Contacts(models.Model):
     opening_hours = models.TimeField()
     closing_hours = models.TimeField()
+
+    class Meta:
+        verbose_name = "Contacts"
+        verbose_name_plural = "Contacts"
+
 
 class Phone(models.Model):
     contacts = models.ForeignKey(Contacts, on_delete=models.DO_NOTHING) 
     number = models.CharField(max_length=25, blank=False, unique=True)
 
+    class Meta:
+        verbose_name = "Phone"
+        verbose_name_plural = "Phones"
+
 class Email(models.Model):
     contacts = models.ForeignKey(Contacts, on_delete=models.DO_NOTHING)
     email = models.EmailField(unique=True, max_length=255, blank=False)
 
+    class Meta:
+        verbose_name = "Email"
+        verbose_name_plural = "Emails"
+
 class Address(models.Model):
     contacts = models.ForeignKey(Contacts, on_delete=models.DO_NOTHING)
     address = models.CharField(max_length=255, blank=False)
+
+    class Meta:
+        verbose_name = "Address"
+        verbose_name_plural = "Address"
